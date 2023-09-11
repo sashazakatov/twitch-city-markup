f713e7ba107e4a32b8eda39a580cbe16ef25afe9
Olexander Zakatov <sashazakatov17@gmail.com>
2023-09-11 21:59:01 +0300

    update

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,10 +1,23 @@
 const swiper = new Swiper('.swiper', {
-  slidesPerView: 3,
+  slidesPerView: 2,
   centeredSlides: true,
   spaceBetween: 15,
   initialSlide: 1,
-  autoplay: {
-   delay: 3000,
+  loop: true,
+  mousewheel: true,
+  grabCursor: true,
+
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+  scrollbar: {
+    el: '.swiper-scrollbar',
+    draggable: true,
   },
   pagination: {
     el: '.swiper-pagination',
