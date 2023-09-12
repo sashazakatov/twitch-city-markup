5ff0fe5599a1aafdfc7ce1a0032a7ab2767f0a5f
Olexander Zakatov <sashazakatov17@gmail.com>
2023-09-12 09:22:14 +0300

    update modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,24 @@
 (() => {
     const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
+      openModalHeaderBtn: document.querySelector("[data-modal-open]"),
+      openModalBtn: document.querySelector(".mobile-contact"),
       closeModalBtn: document.querySelector("[data-modal-close]"),
       modal: document.querySelector("[data-modal]"),
-    };
+  };
   
+    console.log(refs.openModalBtn)
+  
+  refs.openModalHeaderBtn.addEventListener("click", toggleModal);
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
   
-    function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
+  function toggleModal() {
+    
+    const modal = document.querySelector('.is-open');
+    if (modal) { 
+      modal.classList.remove('is-open');
     }
+    
+    refs.modal.classList.toggle("is-hidden");
+  }
   })();
